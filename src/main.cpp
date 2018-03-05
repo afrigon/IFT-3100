aa89d391214ffa7c0fc64db1a597828c15cc09b1
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2018-03-04 19:53:28 -0500

    Rebase repo with openframework

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
-/*
- * Copyright (c) 2018 Alexandre Frigon / Alexandre Rouleau
- *
- * Use of this source code is governed by a MIT license that can be
- * found in the LICENSE file.
- */
+#include "ofMain.h"
+#include "ofApp.h"
 
-#include "Loop.hpp"
+//========================================================================
+int main( ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
-int main() {
-    Loop* loop = new Loop();
-    if (!loop->init()) { return 1; }
-    loop->start();
-    return 0;
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
 }
